b57e86b83adc5aa2da83a9b974c003e48d92f0ac
seven <931744496@qq.com>
2019-04-08 14:42:35 +0800

    沿着path运动

--- src/views/svg/animate.vue
+++ src/views/svg/animate.vue
@@ -6,7 +6,7 @@
         <animate id="c1"
                  attributeName="width"
                  attributeType="XML"
-                 from="200" to="20"
+                 to="20"
                  begin="0s" dur="5s"
                  repeatCount="indefinite"
                  fill="freeze">
@@ -22,6 +22,7 @@
         </animate>
       </circle>
     </svg>
+  
     <h4>多边形和path动画</h4>
     <svg>
       <polygon points="30 30 70 30 90 70 10 70" style="fill: #fcc; stroke: black;">
@@ -29,7 +30,8 @@
                  attributeType="XML"
                  to="50 30 70 50 50 90 30 50"
                  repeatCount="indefinite"
-                 begin="0s" dur="5s" fill="freeze"></animate>
+                 begin="0s" dur="5s" fill="freeze">
+        </animate>
       </polygon>
       <path d="M15 50 Q40 15,50 50,65 32,100 40" style="fill:#fcc;stroke:black" transform="translate(0,50)">
         <animate
@@ -37,24 +39,32 @@
             attributeType="XML"
             to="M50 15Q15 40,50 50,32 65,40 100"
             begin="0s" dur="5s" fill="freeze"
-            repeatCount="indefinite"
-        ></animate>
+            repeatCount="indefinite">
+        </animate>
       </path>
     </svg>
+  
     <h4>沿着path运动</h4>
+    <h5>沿着path运动1</h5>
     <svg width="200" height="200">
-      <!--<rect x="-10" y="-10" width="20" height="20" style="fill:#ff9;stroke:black">-->
-        <!--<animateMotion-->
-            <!--path="M50,135C100,25 150,225 200,125"-->
-            <!--dur="6s" fill="freeze"-->
-            <!--repeatCount="indefinite"-->
-            <!--rotate="auto"-->
-        <!--&gt;</animateMotion>-->
-      <!--</rect>-->
+      <rect x="-10" y="-10" width="20" height="20" style="fill:#ff9;stroke:black">
+        <animateMotion
+            path="M50,135C100,25 150,225 200,125"
+            dur="6s" fill="freeze"
+            repeatCount="indefinite"
+            rotate="auto">
+        </animateMotion>
+      </rect>
+    </svg>
+    <h5>沿着path运动2</h5>
+    <svg width="200" height="200">
       <path id="movePath" d="M50,135C100,25 150,225 200,125" fill="none" stroke="black"></path>
       <rect x="-10" y="-10" width="20" height="20" style="fill:#ff9;stroke:black">
-        <animateMotion dur="6s" fill="freeze" rotate="auto">
-          <mPath xlink:href="#movePath"></mPath>
+        <animateMotion dur="6s"
+                       fill="freeze"
+                       rotate="auto"
+                       repeatCount="indefinite">
+          <mpath xlink:href="#movePath"></mpath>
         </animateMotion>
       </rect>
     </svg>
@@ -63,11 +73,9 @@
 
 <script>
   export default {
-    name: '',
+    name: 'vAnimate',
     data() {
-      return {
-        msg: ''
-      };
+      return {};
     },
     methods: {}
   };
